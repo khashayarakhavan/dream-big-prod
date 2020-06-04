2dd2d03279458a29b865f3b6eae297cb741af31a
AftoflBIG5 <AftoflBIG5@gmail.com>
2020-06-04 08:58:51 -0800

    Solve Up-Button hover effect

--- client/src/components/Static/UpButton.js
+++ client/src/components/Static/UpButton.js
@@ -8,38 +8,6 @@ const Template = () => {
           <span className="UpButton__icon">&nbsp;</span>
         </a>
 
-        <div className="UpButton__background">&nbsp;</div>
-
-        {/* Here is the section which covers the whole page when toggle is clicked.  */}
-        <nav className="UpButton__nav">
-          <ul className="UpButton__list">
-            <li className="UpButton__item">
-              <a href="#" className="UpButton__link">
-                <span>01</span>About
-              </a>
-            </li>
-            <li className="UpButton__item">
-              <a href="#" className="UpButton__link">
-                <span>02</span>Profile
-              </a>
-            </li>
-            <li className="UpButton__item">
-              <a href="#" className="UpButton__link">
-                <span>03</span>Tours
-              </a>
-            </li>
-            <li className="UpButton__item">
-              <a href="#" className="UpButton__link">
-                <span>04</span>Bigger Titles here
-              </a>
-            </li>
-            <li className="UpButton__item">
-              <a href="#" className="UpButton__link">
-                <span>05</span>Book Now
-              </a>
-            </li>
-          </ul>
-        </nav>
       </div>
     );
 };
